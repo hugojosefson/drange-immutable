9d1c1666da10ff349556f61bcefa3a5096656e60
Hugo Josefson <hugo@josefson.org>
2019-03-03 14:33:45 +0000

    Add tests for longer chains, combining single numbers into range.
    Fixes #2

---
+++ test/combine-singles-test.js
@@ -0,0 +1,24 @@
+import { strictEqual } from 'assert'
+import { describe, it } from 'mocha'
+
+import wrapper from '../esmodule'
+import DRange from 'drange'
+
+const range = wrapper(DRange)
+
+describe('combine-singles', () => {
+  describe(`several single numbers combined are seen as one range`, () => {
+    it('in order', () => {
+      const actual = range(1).add(2).add(3).add(4)
+      strictEqual(actual.toString(), '[ 1-4 ]')
+    })
+    it('in reverse order', () => {
+      const actual = range(4).add(3).add(2).add(1)
+      strictEqual(actual.toString(), '[ 1-4 ]')
+    })
+    it('out of order', () => {
+      const actual = range(3).add(1).add(4).add(2)
+      strictEqual(actual.toString(), '[ 1-4 ]')
+    })
+  })
+})
---
+++ test/longer-chain-test.js
@@ -0,0 +1,18 @@
+import { strictEqual } from 'assert'
+import { describe, it } from 'mocha'
+
+import wrapper from '../esmodule'
+import DRange from 'drange'
+
+const range = wrapper(DRange)
+
+describe('longer-chain', () => {
+  it(`longer chain doesn't mutate intermediate instances`, () => {
+    const first = range(1, 100)
+    const second = first.subtract(50, 60)
+    const third = second.add(150)
+    strictEqual(first.toString(), '[ 1-100 ]')
+    strictEqual(second.toString(), '[ 1-49, 61-100 ]')
+    strictEqual(third.toString(), '[ 1-49, 61-100, 150 ]')
+  })
+})
